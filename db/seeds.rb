85ba30978f204a59f87555a330bb8f8c7afb09f6
AlexDNZ <paleozene@gmail.com>
2019-03-12 15:04:37 +0100

    Seeds for cities created Seeds for sports created Seeds for comments created

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,28 @@ require 'faker'
 15.times do
   user_random = User.create!(first_name: Faker::Name.first_name ,last_name: Faker::Name.last_name ,email: Faker::Internet.email, password: Faker::Internet.password(8), gender: Faker::Gender.binary_type , level: "good")
 end
+puts "Users faker generated"
 
 
 # description: Faker::Hipster.sentence ,
 # phone_number :Faker::PhoneNumber.cell_phone
+
+
+10.times do
+  city_random = City.create!(city_name: Faker::Address.city )
+end
+puts "Cities faker generated"
+
+Sport.create!(sport_name: "football" )
+Sport.create!(sport_name: "handball" )
+Sport.create!(sport_name: "pétanque" )
+Sport.create!(sport_name: "running" )
+Sport.create!(sport_name: "tennis" )
+Sport.create!(sport_name: "natation" )
+
+puts "Sport categories generated"
+
+10.times do
+  comment_random = Comment.create!(content: Faker::Hipster.sentence , user_id: rand(1..User.count) , event_id: rand(1..Event.count) )
+end
+puts "Comments faker generated"
