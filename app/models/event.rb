40b109d2f400dcc5e0cc0d47fc6b97d5aa18f861
MargVander <margauxvanderbeken@gmail.com>
2019-03-12 18:22:09 +0100

    search feature ok need to add a date field

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,18 @@
 class Event < ApplicationRecord
-  #belongs_to :city
-  #belongs_to :sport
-  #belongs_to :owner, class_name: "User"
-  #has_many :attendees, through: :attendances
-  #has_many :comments
+  belongs_to :city
+  belongs_to :sport
+  belongs_to :owner, class_name: "User"
+  has_many :attendees, through: :attendances
+  has_many :comments
+
+  def self.search(params)
+    puts params
+    if params["city"] != ""
+    @city_id = City.find_by(city_name: params["city"]).id
+    @sport_id = Sport.find_by(sport_name: params["sport"]).id
+      where(["city_id = ? and sport_id = ?", "#{@city_id}", "#{@sport_id}"])
+    else
+      all
+    end
+  end
 end
