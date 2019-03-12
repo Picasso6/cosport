2173bdbca55bd4791d9b870a7e8da5b687c21007
stephanesdr <stadlerstephane@gmail.com>
2019-03-12 12:52:16 +0100

    add association to events and attendance

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :comments
+  has_many :events, foreign_key: 'owner_id', class_name: "Event"
+  has_many :attendances, foreign_key: 'attendee_id', class_name: "Attendance"
+  has_many :events, through: :attendances
+
 end
