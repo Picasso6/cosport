b2776f10d007343ae83d5d7c0ac747efd64d1157
LordGoblin <maxime78600@gmail.com>
2019-03-13 21:15:29 +0100

    add controller comment end method create // user comment event ok

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,12 @@
 class CommentsController < ApplicationController
+
 	def create
-		
+		@comment = Comment.new(content: params[:content], event_id: params[:event_id], user_id: current_user.id)
+		if @comment.save
+			redirect_to request.referrer
+		else
+			redirect_to request.referrer
+		end
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :events, only: [:show, :index , :new , :create] do
     resources :attendances, only: [:create]
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :new]
   end
 
   resources :users, only: [:show ] do
