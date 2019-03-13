4b5dc74db05cb5f5618c10772c549e664ddba3a7
AlexDNZ <paleozene@gmail.com>
2019-03-13 18:00:27 +0100

    Fix bug with hour_time in events creation(problem with Greenwich Mean Time) Modification in New Event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,40 +9,29 @@ class EventsController < ApplicationController
     @events = Event.search(params)
   end
 
-
   def create
     @event = Event.create
     @event.title = params['title']
     @event.sport_id = params['sport_id']
     @event.description = params["description"]
-    #hour_start = params['hour_start']
     @event.duration = params["duration"]
     puts "**************"
     puts 'HOURS =>'
-
     puts params['hour_start'].values[0].to_time
     hour = params['hour_start'].values[0].to_time.strftime('%H').to_i
     minute = params['hour_start'].values[0].to_time.strftime('%M').to_i
-
     puts "**************"
-
-    @event.start_date = (params["start_date"].to_time +hour.hours+ minute.minutes).to_datetime
-    #@event.start_date = (params["start_date"].to_time + params['hour_start'].values[0].to_time).to_datetime
-
-    #(start_date.to_time + hour.hours + minute.minutes).to_datetime
+    @event.start_date = (params["start_date"].to_time + (hour+1).hours+ minute.minutes).to_datetime
+    puts "date time : #{@event.start_date.asctime}"
     @event.city_id = params["city_id"]
-
-
     @event.owner_id =  current_user.id
     #@event.validated = false
     @event.save
 
     if @event.save
-      #log_in(@user)
       #flash[:success] = "EVENT bien créé !"
       redirect_to event_path(@event.id)
       puts "CRÉATION D'EVENT RÉUSSIE"
-
     else
       #flash.now[:danger] = 'Problème lors de ta création d'evenement '
       puts "$$$$$$$$PROBLEME CRÉATION EVENT $$$$$$$$"
