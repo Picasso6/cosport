ac4e3953986d9d01288bdcf07aab5857a45cb95d
AlexDNZ <paleozene@gmail.com>
2019-03-13 16:57:58 +0100

    debug event creation on heroku

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,7 @@ class EventsController < ApplicationController
     @event.sport_id = params['sport_id']
     @event.description = params["description"]
     #hour_start = params['hour_start']
+    @event.duration = params["duration"]
     puts "**************"
     puts 'HOURS =>'
 
@@ -25,14 +26,14 @@ class EventsController < ApplicationController
 
     puts "**************"
 
-    @event.start_date = (params["start_date"].to_time + hour.hours + minute.minutes).to_datetime
+    @event.start_date = (params["start_date"].to_time + minute.minutes).to_datetime
     #@event.start_date = (params["start_date"].to_time + params['hour_start'].values[0].to_time).to_datetime
 
     #(start_date.to_time + hour.hours + minute.minutes).to_datetime
     @event.city_id = params["city_id"]
 
-    @event.duration = params["duration"]
-    @event.owner_id =  1
+
+    @event.owner_id =  current_user.id
     #@event.validated = false
     @event.save
 
@@ -41,9 +42,11 @@ class EventsController < ApplicationController
       #flash[:success] = "EVENT bien créé !"
       redirect_to event_path(@event.id)
       puts "CRÉATION D'EVENT RÉUSSIE"
+
     else
       #flash.now[:danger] = 'Problème lors de ta création d'evenement '
-      puts "PROBLEME CRÉATION EVENT"
+      puts "$$$$$$$$ PROBLEME CRÉATION EVENT $$$$$$$$"
+      puts @event.errors.full_messages
       render :new
     end
   end
