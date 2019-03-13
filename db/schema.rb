60a938ab9556fb62222c9533cf9952df2756eb0d
stephanesdr <stadlerstephane@gmail.com>
2019-03-13 12:00:59 +0100

    revert schema to db healthy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_112833) do
+ActiveRecord::Schema.define(version: 2019_03_12_113331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2019_03_12_112833) do
     t.string "city_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "event_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,18 @@
 require 'faker'
 
 15.times do
-  user_random = User.new(first_name: Faker::Name.first_name ,last_name: Faker::Name.last_name ,email: Faker::Internet.email, password: Faker::Internet.password(8), gender: Faker::Gender.binary_type , level: "good")
-  user_random.save
+ user_random = User.create!(first_name: Faker::Name.first_name ,last_name: Faker::Name.last_name ,email: Faker::Internet.email, password: Faker::Internet.password(8), gender: Faker::Gender.binary_type , level: "good")
 end
 puts "Users faker generated"
 
-20.times do
-  event = Event.new(title: Faker::Company.bs, description: Faker::Config.random, start_date: Faker::Date.forward(23), duration: 30, owner_id: User.all.sample)
-  event.save
-end
 
-puts "Events faker generated"
 
 # description: Faker::Hipster.sentence ,
 # phone_number :Faker::PhoneNumber.cell_phone
 
 
 10.times do
-  city_random = City.new(city_name: Faker::Address.city )
-  city_random.save
+ city_random = City.create!(city_name: Faker::Address.city )
 end
 puts "Cities faker generated"
 
@@ -40,8 +33,14 @@ Sport.create!(sport_name: "Natation" )
 
 puts "Sport categories generated"
 
-10.times do
-  comment_random = Comment.new(content: Faker::Hipster.sentence , user_id: User.all.sample , event_id: Event.all.sample)
-  comment_random.save
+20.times do
+ event_random = Event.create!(title: Faker::Company.bs, description: Faker::Hipster.sentence, start_date: Faker::Date.forward(23), duration: 30, city_id: rand(1..City.count), sport_id: rand(1..Sport.count) ,owner_id: rand(1..User.count))
 end
+
+puts "Events faker generated"
+
+10.times do
+ comment_random = Comment.create!(content: Faker::Hipster.sentence , user_id: rand(1..User.count),event_id: and(1..Event.count)  )
+end
+
 puts "Comments faker generated"
