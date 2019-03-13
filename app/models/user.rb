6e67d00e549df113dcdc301a5fc125126ff41e75
LordGoblin <maxime78600@gmail.com>
2019-03-13 15:23:53 +0100

    validates Users: description/email/first_name/last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "dont valid email" }
+  validates :first_name, presence: true, length: { in: 3..30}
+  validates :last_name, presence: true, length: { in: 3..30}
+
+  validates :description, length: { in: 3..150}
+
   has_many :comments
   has_many :events, foreign_key: 'owner_id', class_name: "Event"
   has_many :attendances, foreign_key: 'attendee_id', class_name: "Attendance"
