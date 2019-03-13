7b602664312d41d2edebd054d35e95069cff4821
LordGoblin <maxime78600@gmail.com>
2019-03-13 21:57:58 +0100

    variable change: @user.all => @user.find(params (:id) in controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @users = User.all
+  	puts "***************************************************"
+  	puts params
+    @user = User.find(params[:id])
   end
 end
