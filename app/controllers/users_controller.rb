2b4e8f1b0beff44a60e81a30675f8c1c12a83511
LordGoblin <maxime78600@gmail.com>
2019-03-13 22:22:50 +0100

    User to active_storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def show
-  	puts "***************************************************"
-  	puts params
     @user = User.find(params[:id])
   end
 end
