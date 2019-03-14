52c1dfc975cedd0221e2b6c10b0fbafbccb360cc
LordGoblin <maxime78600@gmail.com>
2019-03-14 22:24:44 +0100

    after migrate update models user/users_sports/sport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_111724) do
+ActiveRecord::Schema.define(version: 2019_03_14_210156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,16 @@ ActiveRecord::Schema.define(version: 2019_03_14_111724) do
     t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_sports", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "sport_id"
+    t.string "level_user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sport_id"], name: "index_users_sports_on_sport_id"
+    t.index ["user_id"], name: "index_users_sports_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
