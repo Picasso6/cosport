2d3b08edb258a6aebc6c2bfd7b2e2da58dbe5571
LordGoblin <maxime78600@gmail.com>
2019-03-14 15:22:53 +0100

    cleaning opinions_controller

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,10 +1,6 @@
 class OpinionsController < ApplicationController
 
   def create
-  	puts "////////////////////////////////////"
-  	puts current_user.id
-  	puts params[:user_id].to_i
-  	puts params[:content]
   	@opinion = Opinion.new(content: params[:content], recipient_id: params[:user_id], sender_id: current_user.id)
 	if @opinion.save
 		redirect_to request.referrer
