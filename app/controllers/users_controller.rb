2f4e8d41eb3e613d2d928ed8f68433c3ddb66e95
LordGoblin <maxime78600@gmail.com>
2019-03-14 15:19:08 +0100

    update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @opinion = Opinion.where(recipient_id: @user)
-    puts "**********************************"
-    @opinion.each do |opi|
-    	puts "///////////////////"
-    	puts opi.id
-    end
-    puts "*************************************"
   end
 
 end
