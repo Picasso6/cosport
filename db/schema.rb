022f648b58bed11d4927f1f0731a3bc7466921d8
LordGoblin <maxime78600@gmail.com>
2019-03-14 15:46:09 +0100

    fix bug profil_picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_03_14_102110) do
+ActiveRecord::Schema.define(version: 2019_03_14_111724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
