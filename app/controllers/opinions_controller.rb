0c6124f9f4875e1190c2eb4daac63d95097bbf3b
LordGoblin <maxime78600@gmail.com>
2019-03-14 15:22:16 +0100

    update opinions_controller add method create

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,6 +1,16 @@
 class OpinionsController < ApplicationController
 
   def create
+  	puts "////////////////////////////////////"
+  	puts current_user.id
+  	puts params[:user_id].to_i
+  	puts params[:content]
+  	@opinion = Opinion.new(content: params[:content], recipient_id: params[:user_id], sender_id: current_user.id)
+	if @opinion.save
+		redirect_to request.referrer
+	else
+		redirect_to request.referrer
+	end
   end
-  
+
 end
