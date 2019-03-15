a38bbd1fae17d568f3ce205dbdabe70c03551e39
MargVander <margauxvanderbeken@gmail.com>
2019-03-15 18:32:57 +0100

    updated column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_210156) do
+ActiveRecord::Schema.define(version: 2019_03_15_161155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_210156) do
     t.string "first_name"
     t.string "last_name"
     t.integer "phone_number"
-    t.string "level"
+    t.integer "level"
     t.string "gender"
     t.string "description"
     t.boolean "admin"
