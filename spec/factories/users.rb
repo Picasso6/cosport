ceb15db20521d8c0dc9b4a70d22bb98b0ec14fc7
LordGoblin <maxime78600@gmail.com>
2019-03-16 17:26:23 +0100

    clean fichier spec

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
+    gender { Faker::Gender.binary_type }
+    level { "good" }
+    phone_number { 0666666666 }
+    description { "blabla" }
+
+  end
+end
