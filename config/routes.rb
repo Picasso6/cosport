1f3ca1c52669cd74a3bfb9b19bea8ff7ced526fb
stephanesdr <stadlerstephane@gmail.com>
2019-03-17 17:30:28 +0100

    modify somes routes for check issue on search bar and rendering good layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'events/show'
-  get 'events/index'
+  # get 'events/show'
+  # get 'events/index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
