5f4befd0e574d19e9c815bb717f402f7c3f341a3
LordGoblin <maxime78600@gmail.com>
2019-03-18 16:56:21 +0100

    spec models event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -16,10 +16,42 @@ RSpec.describe Event, type: :model do
       expect(@event).to be_a(Event)
     end
 
+    describe "#title" do
+      it { expect(@event.title).to be_a(String) }
+      it { expect(@event).to validate_presence_of(:title) }
+      it { expect(@event).to validate_length_of(:title).is_at_most(20) }
+    end
+
+    describe "#description" do
+      it { expect(@event.description).to be_a(String) }
+      it { expect(@event).to validate_presence_of(:description) }
+      it { expect(@event).to validate_length_of(:description).is_at_most(500) }
+    end
+
+    describe "#start_date" do
+      it { expect(@event.start_date).to be_a(Time) }
+      it { expect(@event).to validate_presence_of(:start_date) }
+    end
 
   end
 
-  context "associations" do
+   context "associations" do
+
+    describe "#owner" do
+      it { expect(@event).to belong_to(:owner) }
+    end
+
+    describe "#city" do
+      it { expect(@event).to belong_to(:city) }
+    end
+
+    describe "#sport" do
+      it { expect(@event).to belong_to(:sport) }
+    end
+
+    describe "#sport" do
+      it { expect(@event).to belong_to(:sport) }
+    end
 
   end
 end
