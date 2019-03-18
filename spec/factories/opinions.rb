89e37731f56fae2054bf042bf17026e84360dacd
LordGoblin <maxime78600@gmail.com>
2019-03-18 15:45:05 +0100

    spec opinions

--- spec/factories/opinions.rb
+++ spec/factories/opinions.rb
@@ -1,5 +1,8 @@
+
 FactoryBot.define do
   factory :opinion do
-    
+	content { "bla bla le sport c'est nul"} 
+	sender { FactoryBot.create(:user) }
+	recipient { FactoryBot.create(:user) }
   end
 end
---
+++ spec/models/opinion_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Opinion, type: :model do
+
+   before(:each) do 
+     @opinion = FactoryBot.create(:opinion)
+     @user = FactoryBot.create(:user)
+   end
+  
+  it "has a valid factory" do
+    expect(build(:opinion)).to be_valid
+    expect(build(:user)).to be_valid
+  end
+
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@opinion).to be_a(Opinion)
+    end
+
+    describe "#content" do
+      it { expect(@opinion.content).to be_a(String) }
+      it { expect(@opinion).to validate_presence_of(:content) }
+      it { expect(@opinion).to validate_length_of(:content).is_at_least(10) }
+      it { expect(@opinion).to validate_length_of(:content).is_at_most(200) }
+    end
+
+  end
+
+  context "associations" do
+
+    describe "#sender" do
+      it { expect(@opinion).to belong_to(:sender) }
+    end
+
+    describe "#recipient" do
+      it { expect(@opinion).to belong_to(:recipient) }
+    end
+
+  end
+end
