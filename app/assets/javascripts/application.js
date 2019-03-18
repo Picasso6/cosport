a4979bee396783b5bc629ceb9a8fd2ca97e9eaec
stephanesdr <stadlerstephane@gmail.com>
2019-03-18 18:33:46 +0100

    update front navbar and searchbar (fix aligns input and select field)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,11 +20,11 @@
 
 
 
-$(window).scroll(function() {
-  /* affix after scrolling 100px */
-  if ($(document).scrollTop() > 100) {
-    $('.navbar').addClass('affix');
-  } else {
-    $('.navbar').removeClass('affix');
-  }
-});
+// $(window).scroll(function() {
+//   /* affix after scrolling 100px */
+//   if ($(document).scrollTop() > 100) {
+//     $('.navbar').addClass('affix');
+//   } else {
+//     $('.navbar').removeClass('affix');
+//   }
+// });
