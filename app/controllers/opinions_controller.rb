5e2196431153bfab375d8864d349865d029f6760
LordGoblin <maxime78600@gmail.com>
2019-03-19 17:16:10 +0100

    spec controller opinions create/destroy

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -2,17 +2,16 @@ class OpinionsController < ApplicationController
 
   def create
   	@opinion = Opinion.new(content: params[:content], recipient_id: params[:user_id], sender_id: current_user.id)
-	if @opinion.save
-		redirect_to request.referrer
-	else
-		redirect_to request.referrer
-	end
+  	if @opinion.save
+  		redirect_to request.referrer
+  	else
+  		redirect_to request.referrer
+  	end
   end
 
   def destroy
-  	puts "*****************************"
-  	puts params
-	@opinion = Opinion.find(params[:id])
+
+	  @opinion = Opinion.find(params[:id])
     @opinion.destroy
     redirect_to request.referrer
   end
---
+++ spec/controllers/opinions_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe OpinionsController, type: :controller do
+
+ 	describe 'POST #create' do
+
+ 		context 'invalid params' do
+
+ 			before(:each) do
+
+ 				login_with FactoryBot.create( :user )
+ 				request.env['HTTP_REFERER'] = '/'
+ 				post :create, params: {user_id: FactoryBot.create( :user ), recipient_id: FactoryBot.create( :user ).id, content: "balbalbalbalbalblablabla"}
+
+ 			end
+
+ 			it 'should return an error' do 				
+ 				expect(response).to have_http_status(302)
+ 			end
+
+ 		end
+
+ 	end
+
+ 	describe 'POST #destroy' do
+
+ 		context 'invalid params' do
+
+ 			before(:each) do
+
+ 				login_with FactoryBot.create( :user )
+ 				request.env['HTTP_REFERER'] = '/'
+ 				post :destroy, params: {user_id: FactoryBot.create( :user ), id: FactoryBot.create( :opinion ).id}
+
+ 			end
+
+ 			it 'should return an error' do 				
+ 				expect(response).to have_http_status(302)
+ 			end
+
+ 		end
+
+ 	end
+
+end
