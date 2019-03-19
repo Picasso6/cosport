afbb6f3febd20cb03557911e4ef49b26171aac8b
LordGoblin <maxime78600@gmail.com>
2019-03-19 16:00:45 +0100

    clean controller comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,17 +7,10 @@ class CommentsController < ApplicationController
   	end
 
 	def create
-		puts "*************************************************"
-		puts params
-		puts current_user.id
 		@comment = Comment.new(content: params[:content], event_id: params[:event_id], user_id: current_user.id)
-		puts "////////////////////////////////////////////////////"
-		puts @comment.user_id
 		if @comment.save
-			puts "ififififiifififififiifififififiififif"
 			redirect_to request.referrer
 		else
-			puts "elellelelelellelelellelelellelelelele"
 			redirect_to request.referrer
 		end
 	end
