808da63262604c1014e7cbb8936b7a5be196e2f4
AlexDNZ <paleozene@gmail.com>
2019-03-20 16:42:36 +0100

    modif to fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,7 +14,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile = ['*.erb']
 Rails.application.config.assets.precompile = ['*.js']
-Rails.application.config.assets.precompile += ['edge/ink-falling_edge.js']
+#Rails.application.config.assets.precompile += ['edge/ink-falling_edge.js']
 Rails.application.config.assets.precompile += %w( new_event_map.js.erb )
 Rails.application.config.assets.precompile += %w( show_event_map.js.erb )
 Rails.application.config.assets.precompile += %w( home.js )
