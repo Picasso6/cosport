1fc1a47e3e76d527298353c914462a2411d20cfa
AlexDNZ <paleozene@gmail.com>
2019-03-20 14:04:48 +0100

    debu heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-# Rails.application.config.assets.precompile += %w( new_event_map.js )
-# Rails.application.config.assets.precompile += %w( show_event_map.js )
-# Rails.application.config.assets.precompile += %w( home.js )
-# Rails.application.config.assets.precompile += %w( main_map.js )
+Rails.application.config.assets.precompile += %w( new_event_map.js )
+Rails.application.config.assets.precompile += %w( show_event_map.js )
+Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( main_map.js )
