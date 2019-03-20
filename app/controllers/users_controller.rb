d4d3d2d6be939127aefcae94c5def7a093625bad
MargVander <margauxvanderbeken@gmail.com>
2019-03-20 15:26:18 +0100

    sorted event and attendance so it look cleaner in the user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:new , :show]
-  config.filter_parameters << :secret_key
+  # config.filter_parameters << :secret_key
 
   def index
     @user = User.all
@@ -11,12 +11,27 @@ class UsersController < ApplicationController
   	@level_sport = UsersSport.level_sport
     @user = User.find(params[:id])
     @opinion = Opinion.where(recipient_id: @user)
-    @events = []
-    Event.where(owner_id: current_user.id).each {|evt| @events << evt }
-    Attendance.where(attendee_id: current_user.id, validation: true).each {|atd| @events << Event.find(atd.event_id)}
-    @events_hash = @events.group_by_day { |evt| evt.start_date }
+
+    @future_owned_events = Event.where("owner_id = ? and start_date >= ?", "#{current_user.id}", "#{DateTime.now}")
+    @future_owned_events_hash = @future_owned_events.group_by_day { |evt| evt.start_date }
+    @past_owned_events = Event.where("owner_id = ? and start_date <= ?", "#{current_user.id}", "#{DateTime.now}")
+    @past_owned_events_hash = @past_owned_events.group_by_day { |evt| evt.start_date }
+    @user_attendances = Attendance.where("attendee_id = ?", "#{current_user.id}").sort { |a,b| a.event.start_date <=> b.event.start_date }
+    @future_attendances = []
+    @past_attendances = []
+    @user_attendances.each do |attendance|
+      if attendance.event.start_date >= DateTime.now
+        @future_attendances << attendance
+      else
+        @past_attendances << attendance
+      end
+    end
+    @future_attendances_hash = @future_attendances.group_by_day { |att| att.event.start_date }
+    @past_attendances_hash = @past_attendances.group_by_day { |att| att.event.start_date }
     @months_array_fr = [ nil, "Janvier", "Fevrier" ," Mars" , "Avril" , "Mai" , "Juin" ,"Juillet", "Août" , "Septembre" , "Octobre" , "Novembre" ,"Décembre"]
     @days_array_fr = ["Dimanche" , "Lundi "," Mardi" , "Mercredi" ,"Jeudi" , "Vendredi", "Samedi" ]
+
+
     if @user.level >= 5 && @user.level <50
       @level = "Curieux"
     elsif @user.level >= 50 && @user.level < 200
