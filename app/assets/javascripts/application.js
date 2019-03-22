c74b5870fc9f6c2f66aa6fa06d7b18df58adca94
AlexDNZ <paleozene@gmail.com>
2019-03-22 18:22:33 +0100

    replace jquery require in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery_ujs
 //= require leaflet
 //= require new_event_map
 //= require show_event_map
 //= require main_map
 //= require turbolinks
 //= require bootstrap
-//= require jquery3
-//= require jquery_ujs
 //= require popper
 //= require activestorage
 //= require_tree .
