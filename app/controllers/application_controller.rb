59bf6332b1197d290094326ab1d7238ae361c852
LordGoblin <maxime78600@gmail.com>
2019-03-23 04:19:39 +0100

    position front for chatbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :tchatbox
+
+	def tchatbox
+    session[:conversations] ||= []
+
+    @users = User.all.where.not(id: current_user)
+    @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
+	end
 end
