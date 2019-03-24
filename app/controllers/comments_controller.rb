dd325959620d2586060f23461ec172a29b062dd3
LordGoblin <maxime78600@gmail.com>
2019-03-24 18:02:41 +0100

    update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,29 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(content: params[:content], event_id: params[:event_id], user_id: current_user.id)
+    if @comment.save
+      respond_to do |format|
+        # flash[:notice] = "Le commentaire a bien été envoyé."
+        # redirect_to request.referrer
+        format.html { redirect_to request.referrer }
+        format.js
+      end
+    else
+      puts @comment.errors.full_messages
+      flash[:danger] = "Erreur lors de l'envoi du commentaire'."
+      redirect_to request.referrer, status: :unprocessable_entity
+    end
+  end
 
-	def create
-		@comment = Comment.new(content: params[:content], event_id: params[:event_id], user_id: current_user.id)
-		if @comment.save
-			 respond_to do |format|
-			# flash[:notice] = "Le commentaire a bien été envoyé."
-			# redirect_to request.referrer
-			format.html { redirect_to request.referrer }
-			 format.js
-		end
-		else
-			puts @comment.errors.full_messages
-			flash[:danger] = "Erreur lors de l'envoi du commentaire'."
-			redirect_to request.referrer, status: :unprocessable_entity
-		end
-	end
-
-	def destroy
-		@comment = Comment.find(params[:id])
-    	if @comment.destroy
-				respond_to do |format|
-				format.html { redirect_to request.referrer }
-				format.js
-			end
-		end
-	end
-
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to request.referrer }
+        format.js
+      end
+      end
+  end
 end
