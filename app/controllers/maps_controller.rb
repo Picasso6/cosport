6be79271f3557163a8443f85894f538731b475fa
AlexDNZ <paleozene@gmail.com>
2019-03-24 01:14:52 +0100

    Debug map icon zoom Add a time condition to event selected for main_map feature

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,7 +2,7 @@ class MapsController < ApplicationController
 
   def index
     @events = Event.all
-    gon.events = @events
+    gon.eventsToday = @events.where(start_date: (Time.now.utc+ Time.now.gmt_offset).utc..(Time.now.utc+ Time.now.gmt_offset).utc.end_of_day)
   end
 
 end
