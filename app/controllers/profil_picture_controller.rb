9d40d83a7b8c257754037f86508129bbda63daf9
LordGoblin <maxime78600@gmail.com>
2019-03-24 18:04:36 +0100

    profil picture controller

--- app/controllers/profil_picture_controller.rb
+++ app/controllers/profil_picture_controller.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class ProfilPictureController < ApplicationController
-
-	def create
-		@user = User.find(params[:user_id])
-		@user.profil_picture.attach(params[:profil_picture])
-		redirect_to(edit_user_registration_path)
-	end
-
+  def create
+    @user = User.find(params[:user_id])
+    @user.profil_picture.attach(params[:profil_picture])
+    redirect_to(edit_user_registration_path)
+  end
 end
