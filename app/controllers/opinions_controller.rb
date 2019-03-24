fc03b98d2ac0722f9b2ed50ae03638a6f3584163
LordGoblin <maxime78600@gmail.com>
2019-03-24 18:04:14 +0100

    update opinions controller

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,20 +1,18 @@
-class OpinionsController < ApplicationController
+# frozen_string_literal: true
 
+class OpinionsController < ApplicationController
   def create
-  	@opinion = Opinion.new(content: params[:content], recipient_id: params[:user_id], sender_id: current_user.id)
-  	if @opinion.save
-  		redirect_to request.referrer
-  	else
-  		redirect_to request.referrer, status: :unprocessable_entity
-  	end
+    @opinion = Opinion.new(content: params[:content], recipient_id: params[:user_id], sender_id: current_user.id)
+    if @opinion.save
+      redirect_to request.referrer
+    else
+      redirect_to request.referrer, status: :unprocessable_entity
+    end
   end
 
   def destroy
-
-	  @opinion = Opinion.find(params[:id])
+    @opinion = Opinion.find(params[:id])
     @opinion.destroy
     redirect_to request.referrer
   end
-
 end
-
