992b00c1b84e4080a59c7d853de67c9706635ffc
LordGoblin <maxime78600@gmail.com>
2019-03-24 18:03:23 +0100

    update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-  	@user = User.all
-  	@event = Event.all
+    @user = User.all
+    @event = Event.all
     @events_last = Event.order('created_at DESC')[0..3]
-    puts "$" * 200
-    puts "params"
+    puts '$' * 200
+    puts 'params'
     # @events_landing = events_last
-
   end
 end
