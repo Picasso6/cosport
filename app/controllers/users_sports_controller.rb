6c529a9efdd9a3b43ec640002515ceb1021162d3
LordGoblin <maxime78600@gmail.com>
2019-03-24 18:05:44 +0100

    update users sport controller

--- app/controllers/users_sports_controller.rb
+++ app/controllers/users_sports_controller.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 class UsersSportsController < ApplicationController
-  
   def create
-  	@user_sport = UsersSport.new(user_id: current_user.id, sport_id: params[:sport], level_user: params[:level])
-  	if @user_sport.save
-  		redirect_to request.referrer
-  	else
-  		redirect_to request.referrer
-  	end
+    @user_sport = UsersSport.new(user_id: current_user.id, sport_id: params[:sport], level_user: params[:level])
+    if @user_sport.save
+      redirect_to request.referrer
+    else
+      redirect_to request.referrer
+    end
   end
 
   def destroy
-	@user_sport = UsersSport.find(params[:id])
+    @user_sport = UsersSport.find(params[:id])
     @user_sport.destroy
     redirect_to request.referrer
   end
-
 end
