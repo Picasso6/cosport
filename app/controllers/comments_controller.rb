2022b061ca6117f3ea2284251a16afb30067d083
AlexDNZ <paleozene@gmail.com>
2019-03-24 16:01:03 +0100

    remove second call of application.js in layouts/application.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+ 
 	def create
 		@comment = Comment.new(content: params[:content], event_id: params[:event_id], user_id: current_user.id)
 		if @comment.save
